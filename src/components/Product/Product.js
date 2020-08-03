02d1dec51cceb90282025ba79472d44153e4013d
StephenLespron <stephen.lespron@gmail.com>
2020-08-03 16:44:20 -0700

    added static build

--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -1,28 +1,28 @@
-import React from "react";
-import { Link } from "react-router-dom";
-import "./Product.css";
+import React from 'react';
+import { Link } from 'react-router-dom';
+import './Product.css';
 
 export default function Product(props) {
-  const product = props.inventory.map((prod) => {
-    return (
-      <div key={[prod.id]} className="Product">
-        <div className="imgContainer">
-          <img alt="" src={prod.img} className="prodImg" />
-        </div>
-        <div className="descBox">
-          <div className="namePrice">
-            <h3>{prod.name}</h3>
-            <p>${prod.price}</p>
-          </div>
-          <div className="btnBox">
-            <Link to={`/form/${prod.id}`}>
-              <button>Edit</button>
-            </Link>
-            <button onClick={() => props.delete(prod.id)}>Delete</button>
-          </div>
-        </div>
-      </div>
-    );
-  });
-  return <div>{product}</div>;
+	const product = props.inventory.map((prod) => {
+		return (
+			<div key={[prod.id]} className='Product'>
+				<div className='imgContainer'>
+					<img alt={prod.name} src={prod.img} className='prodImg' />
+				</div>
+				<div className='descBox'>
+					<div className='namePrice'>
+						<h3>{prod.name}</h3>
+						<p>${prod.price}</p>
+					</div>
+					<div className='btnBox'>
+						<Link to={`/form/${prod.id}`}>
+							<button>Edit</button>
+						</Link>
+						<button onClick={() => props.delete(prod.id)}>Delete</button>
+					</div>
+				</div>
+			</div>
+		);
+	});
+	return <div>{product}</div>;
 }
